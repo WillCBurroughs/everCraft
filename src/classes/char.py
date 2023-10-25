76b2fa00b4e42af770d99cd154ab694db532a122
Jett Pennington <jettpennington@gmail.com>
2023-10-25 20:26:09 +0000

    Inital Commit

---
+++ src/classes/char.py
@@ -0,0 +1,4 @@
+class Char:
+
+    def __init__(self, name):
+        self.name = name
---
+++ src/tests/test_char.py
@@ -0,0 +1,5 @@
+from classes.char import Char
+
+def test_make_name():
+    charactor = Char("jett")
+    assert charactor.name is not None
